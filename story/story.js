6d59671a64445526335de109c5beef977b5976ed
carolbuckingham <91283833+carolbuckingham@users.noreply.github.com>
2022-02-03 00:44:39 -0500

    Update story.js
    add markers to map, change center point and zoom

--- story/story.js
+++ story/story.js
@@ -1,10 +1,38 @@
 // This is where I will put my js for the custom map
 
-// [START maps_layer_bicycling]
 function initMap() {
-    const map = new google.maps.Map(
-      document.getElementById("map"), {
-        center: { lat: 42.3726399, lng: -71.1096528 },
-        zoom: 14,
-      }
-    );
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 44.245, lng: -76.545 },
+        zoom: 13,
+      });
+    
+  // Javascript for marker accessibility was adapted from: https://developers.google.com/maps/documentation/javascript/examples/marker-accessibility
+  const walkLocs: [google.maps.LatLngLiteral, string][] = [
+    [{ lat: 44.26037915976495, lng: -76.5413786093249 }, "Cataraqui Cemetery"],
+    [{ lat: 44.222401646687636, lng: -76.49854321741287 }, "Breakwater Park"],
+    [{ lat: 44.22579286847256, lng: -76.4815015272718 }, "Battery Park"],
+    [{ lat: 44.2803694367451, lng: -76.56433475010915 }, "Kingston & Pembroke Rail Trail Connector - Centennial"],
+    [{ lat: 44.23346254217058, lng: -76.60991762529308 }, "Lemoine Point"],
+  ];
+
+  // Creates an info window to share between markers
+  const infoWindow = new google.maps.InfoWindow();
+
+  // Creates the markers
+  tourStops.forEach(([position, title], i) => {
+    const marker = new google.maps.Marker({
+      position,
+      map,
+      title: `${i + 1}. ${title}`,
+      label: `${i + 1}`,
+      optimized: false,
+    });
+
+    // Adds a click listener for each marker, and set up the info window
+    marker.addListener("click", () => {
+      infoWindow.close();
+      infoWindow.setContent(marker.getTitle());
+      infoWindow.open(marker.getMap(), marker);
+    });
+  });
+}
